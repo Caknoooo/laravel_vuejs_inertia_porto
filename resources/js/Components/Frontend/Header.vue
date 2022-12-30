aa98079db3869a5e2c3961ecc377911c824b6e5d
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2022-12-30 19:46:58 +0700

    refactor: modify some file to synchronized it

--- resources/js/Components/Frontend/Header.vue
+++ resources/js/Components/Frontend/Header.vue
@@ -8,8 +8,8 @@ const showMobileMenu = ref('false');
 <nav class="bg-light-primary border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-dark-primary">
     <div class="container flex flex-wrap items-center justify-between mx-auto">
         <a href="https://flowbite.com/" class="flex items-center">
-            <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
-            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+            <img src="" class="h-6 mr-3 sm:h-9" alt="Logo" />
+            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">My Blog</span>
         </a>
         <button @click="showMobileMenu = !showMobileMenu" data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
             <span class="sr-only">Open main menu</span>
@@ -18,10 +18,10 @@ const showMobileMenu = ref('false');
         <div class="w-full md:block md:w-auto" :class="{hidden: showMobileMenu}" id="navbar-default">
             <ul class="flex flex-col p-4 mt-4 bg-light-secondary border border-light-tail-500 dark:border-dark-navy-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-dark-secondary md:dark:bg-gray-900">
                 <li>
-                <a href="#" class="block py-2 pl-3 pr-4 text-white bg-light-tail-500 dark:bg-dark-navy-100 rounded md:bg-transparent md:text-blue-700 md:p-0 " aria-current="page">Home</a>
+                <a href="#home" class="block py-2 pl-3 pr-4 text-white bg-light-tail-500 dark:bg-dark-navy-100 rounded md:bg-transparent md:text-blue-700 md:p-0 " aria-current="page">Home</a>
                 </li>
                 <li>
-                <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+                <a href="#about" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
                 </li>
                 <li>
                 <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,13 +1,26 @@
 <script setup>
 
 import { Head, Link } from '@inertiajs/inertia-vue3';
-import Frontend from "@/Layouts/Frontend.vue";
+import FrontendLayout from "@/Layouts/Frontend.vue";
+import Hero from '@/Components/Frontend/Hero.vue'
+import Promote from '@/Components/Frontend/Promote.vue';
+import About from '@/Components/Frontend/About.vue';
 
 </script>
 
 <template>
 
     <Head title="Welcome to portofolio" />
-    <Frontend></Frontend>
+    <FrontendLayout>
+        <!-- Hero Primary -->
+        <Hero />
+
+        <!-- Promote -->
+        <Promote />
+
+        <!-- About -->
+        <About />
+
+    </FrontendLayout>
 
 </template>

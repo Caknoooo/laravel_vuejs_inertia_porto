5e5f75f489d31ef82f3d7d56d20fea671188200f
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2022-12-30 21:01:37 +0700

    feat: add new Portofolio page

---
+++ resources/js/Components/Frontend/Portofolio.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+import Projects from "./Projects.vue";
+defineProps({
+    skills: Object,
+    projects: Object,
+});
+
+</script>
+
+<template>
+    <section
+        id="portfolio"
+        class="section bg-light-primary dark:bg-dark-primary min-h-[1400px]"
+    >
+        <div
+        class="container mx-auto"
+        v-motion
+        :initial="{
+            opacity: 0,
+            y: 100,
+        }"
+        :visible="{
+            opacity: 1,
+            y: 0,
+        }"
+        >
+        <div class="flex flex-col items-center text-center">
+            <h2 class="section-title">My latest work</h2>
+            <p class="subtitle">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga veniam
+            labore nisium illum cupiditate reiciendis a numquam
+            </p>
+        </div>
+        </div>
+        <Projects :skills="skills" :projects="projects" />
+    </section>
+</template>

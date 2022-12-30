660040372fe63dfb85cbf0c59c1f8b1f12b26165
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2022-12-30 14:11:30 +0700

    refactor: styling UI for Header and Footer

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,11 @@
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap"        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;1,400&family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Unbounded:wght@300&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;1,100;1,200;1,300;1,400&display=swap" rel="stylesheet">
+
 
         <!-- Scripts -->
         @routes

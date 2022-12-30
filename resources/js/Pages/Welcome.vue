9e5cc85f21d404e9e3c4e13e532f7cea0eb95d27
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2022-12-30 22:23:14 +0700

    refactor: connected ContactMe with UI and routes

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -7,6 +7,8 @@ import Promote from '@/Components/Frontend/Promote.vue';
 import About from '@/Components/Frontend/About.vue';
 import Skills from '@/Components/Frontend/Skills.vue';
 import Portofolio from '@/Components/Frontend/Portofolio.vue';
+import Services from '@/Components/Frontend/Services.vue';
+import ContactMe from '@/Components/Frontend/ContactMe.vue';
 
 defineProps({
     skills: Object,
@@ -32,6 +34,12 @@ defineProps({
 
         <!-- Portofolio -->
         <Portofolio :skills="skills" :projects="projects" />
+
+        <!-- Services -->
+        <Services />
+
+        <!-- Contact -->
+        <ContactMe />
     </FrontendLayout>
 
 </template>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\SkillController;
@@ -20,6 +21,7 @@ use Inertia\Inertia;
 */
 
 Route::get('/', [WelcomeController::class, 'welcome'])->name('welcome');
+Route::post('/contact', ContactController::class)->name('contact');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', function () {

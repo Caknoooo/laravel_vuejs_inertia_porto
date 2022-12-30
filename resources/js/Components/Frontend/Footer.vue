7b30e7cd8ffd3a27ba8c42f1af63279c33ae69c8
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2022-12-30 14:07:56 +0700

    feat: create Footer views

---
+++ resources/js/Components/Frontend/Footer.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+    <footer class="bg-light-secondary dark:bg-dark-secondary py-12">
+        <div class="container mx-auto">
+            <div class="flex flex-col md:flex-row space-y-6 lg:space-y-0 items-center justify-between">
+                <div class="flex space-x-6 items-center justify-center">
+                    <img class="h-8 w-auto" src="https://flowbite.com/docs/images/logo.svg" alt="Test Logo"  />
+                </div>
+                <p class="text-paragraph opacity-80 text-[15px]">
+                    &copy; 2022 Cakno Blog All Right Reserved.
+                </p>
+            </div>
+        </div>
+    </footer>
+</template>

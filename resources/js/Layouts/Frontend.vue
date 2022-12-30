5f6e16c9a62d835e60eee7e905e82cc35cf95884
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2022-12-30 14:08:50 +0700

    feat: create Frontend Views to setting header and footer

---
+++ resources/js/Layouts/Frontend.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+import Header from '@/Components/Frontend/Header.vue';
+import Footer from '@/Components/Frontend/Footer.vue';
+
+</script>
+
+<template>
+    <div class="bg-slate-200 dark:bg-slate-900">
+        <!-- Header -->
+        <Header />
+        <main class="min-h-screen">
+            <slot />
+        </main>
+        <!-- Footer -->
+        <Footer/>
+    </div>
+</template>
